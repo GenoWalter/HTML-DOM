cd6c300e187a132fa84855c84a1b753cb98e8dfd
Your Name <you@example.com>
2022-04-08 22:32:42 +0700

    ES6

---
+++ ECMAScript 6/ArrowFunction.js
@@ -0,0 +1,10 @@
+// Bạn có thể tạo hàm bằng cách sử dụng dấu mũi tên =>.
+//  Arrow là một dạng viết tắt của các function sử dụng dấu =>, tương tự như trong C#, Java 8,…
+var functionName = (var1, var2) => {
+    // Nội dung function
+};
+
+var hello = () => {
+    console.log('Hello Bạn');
+  };
+  hello();
---
+++ ECMAScript 6/Templateliterals.js
@@ -0,0 +1,12 @@
+//  Template literals
+//  Multi-line String
+
+const courseName = 'Javascript';
+const courseName2= 'PHP';
+const description = `CourseName: ${courseName} ${courseName2}`;
+console.log(description);
+
+const lines = `Line 1
+line 2
+line 3`;
+console.log(lines);
